c9c2aebf8b68ead59d5c61c1d6a683c06e23df97
Gaurav7131 <thakaregaurav7131@gmail.com>
2026-06-08 10:10:47 +0530

    commit

---
+++ Condn.java
@@ -0,0 +1,12 @@
+
+public class Condn {
+    public static void main(String[] args) {
+        Boolean isRaining = true;// cond resolve it we set false it goed down else loop
+
+        if (isRaining) {
+            System.out.println("Its raining");
+        } else {
+            System.out.println("wear Sun coat");
+        }
+    }
+}
---
+++ Method.java
@@ -0,0 +1,13 @@
+class Player {
+    void playing() {
+        System.out.println("Playing");
+    }
+}
+
+public class Method {
+    public static void main(String[] args) {
+        Player p = new Player();
+        p.playing();
+
+    }
+}
