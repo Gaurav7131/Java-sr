7d43a1155d70f467c243afab097242b22d4800c3
Gaurav Thakare <thakaregaurav7131@gmail.com>
2026-09-05 08:37:16 +0530

    Commit Changes

--- Java12FeatPatternMatchingEx.java
+++ Java12FeatPatternMatchingEx.java
@@ -2,13 +2,13 @@ public class Java12FeatPatternMatchingEx {
     public void checkObj(Object obj) {
         // checks types and bind var s directly
         if (obj instanceof String s) {
-            System.out.println("String:" + s.length());// no cast needed
+            System.out.println("String:" + s.length());// no explicit cast needed
         }
     }
 
     public static void main(String[] args) {
         Java12FeatPatternMatchingEx ex = new Java12FeatPatternMatchingEx();
         ex.checkObj("Hiiiii");// 6
-        System.out.println(ex);
+        System.out.println(ex);//hashcode
     }
 }
